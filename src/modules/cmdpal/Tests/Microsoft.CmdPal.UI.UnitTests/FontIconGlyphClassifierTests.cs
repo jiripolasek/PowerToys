e4005f6a98d90355601dc62d58f00063c5183bd9
Jiří Polášek <me@jiripolasek.com>
2026-08-09 07:00:39 +0200

    Optimize managed icon glyph classification

--- src/modules/cmdpal/Tests/Microsoft.CmdPal.UI.UnitTests/FontIconGlyphClassifierTests.cs
+++ src/modules/cmdpal/Tests/Microsoft.CmdPal.UI.UnitTests/FontIconGlyphClassifierTests.cs
@@ -30,6 +30,7 @@ public class FontIconGlyphClassifierTests
     [DataRow("A")]
     [DataRow("\uE6FF")]
     [DataRow("\uF900")]
+    [DataRow("\U0001D11E")]
     [DataRow("e\u0301")]
     public void SingleNonEmojiGraphemesUseTheGeneralFont(string text)
     {
@@ -48,6 +49,7 @@ public class FontIconGlyphClassifierTests
     [DataRow("\u231A")]
     [DataRow("\U0001F600")]
     [DataRow("\u2764\uFE0F")]
+    [DataRow("\u2764\uFE0F\uFE0E")]
     [DataRow("2\uFE0F\u20E3")]
     [DataRow("\U0001F469\u200D\U0001F4BB")]
     public void EmojiGraphemesUseTheEmojiFont(string text)
@@ -58,6 +60,7 @@ public class FontIconGlyphClassifierTests
     [TestMethod]
     [DataRow("\u2764")]
     [DataRow("\u2764\uFE0E")]
+    [DataRow("\u2764\uFE0E\uFE0F")]
     [DataRow("2\u20E3")]
     public void TextPresentationRemainsGeneralText(string text)
     {
